04d9db67d42c7a64b18f927a9e5b505a6998e961
Allan Farca <allan.farca@gmail.com>
2020-04-10 04:36:45 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  
+  def initialize(name, artist, genre)
+    @@count =+ 1 
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end 
